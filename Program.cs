5ac5265127a051cac03193aa9558afa116036b7d
Georgii Naumov <gmnaumov@yandex.ru>
2024-01-12 20:39:18 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ApplicationXmlConfiguration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
